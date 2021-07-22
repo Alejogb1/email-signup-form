de4d6371745a058993773c1e4c6c69d2234afd12
alejogb1 <devsalejo@gmail.com>
2021-07-22 11:02:09 -0300

    Create service.js

---
+++ src/service.js
@@ -0,0 +1,19 @@
+
+
+
+  async function postData (){
+    await fetch('https://api.staging.fourthwall.com/api/mailing-list', {
+      method: 'POST',
+      headers: {
+        'X-ShopId' : 'sh_9f57832f-456b-44f3-888f-8a370b155a18',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: {"email" : "test@email.com"}
+    });
+     
+  }  
+
+  function redirect() {
+    
+  }
